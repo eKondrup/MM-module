ba41d17ea6752c418944113ce5a6b511c51d6a41
Cwoasount <78689800+eKondrup@users.noreply.github.com>
2022-03-20 18:11:52 +0100

    Update MitModule2.js

--- MitModule2.js
+++ MitModule2.js
@@ -1,1 +1,12 @@
-//inset stuff
+//required
+Module.register("mitModule2", {
+	getDom: function() {
+		//const middleCenter = document.querySelector('top center')
+		const wrapper = document.createElement('div')
+		const header = document.createElement('h3')
+		header.innerHTML= "header"
+		wrapper.innerHTML = "Test"
+		wrapper.appendChild(header)
+		return wrapper
+	}
+})
